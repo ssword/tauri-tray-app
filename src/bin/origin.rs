c4c608b4ae3b271b0fcee296130a1f965b1d339a
ssword <ssword@gmail.com>
2022-11-14 23:59:46 +0800

    finished tcp reverse proxy

--- src/bin/origin.rs
+++ src/bin/origin.rs
@@ -5,14 +5,14 @@ use std::str;
 use std::str::FromStr;
 use std::string::ParseError;
 
-#[drive(Debug)]
+#[derive(Debug)]
 struct RequestLine {
     method: Option<String>,
     path: Option<String>,
-    protocal: Option<String>,
+    protocol: Option<String>,
 }
 
-impl ReqeustLine {
+impl RequestLine {
     fn method(&self) -> String {
         if let Some(method) = &self.method {
             method.to_string()
@@ -31,7 +31,95 @@ impl ReqeustLine {
 
     fn get_order_number(&self) -> String {
         let path = self.path();
-        let path_toakens: Vec<String> = path.split("/").map(|s| s.parse().unwrap()).collect();
+        let path_tokens: Vec<String> = path.split("/").map(|s| s.parse().unwrap()).collect();
         path_tokens[path_tokens.len() - 1].clone()
+    }
+}
+
+impl FromStr for RequestLine {
+    type Err = ParseError;
+
+    fn from_str(msg: &str) -> Result<Self, Self::Err> {
+        let mut msg_tokens = msg.split_ascii_whitespace();
+        let method = match msg_tokens.next() {
+            Some(token) => Some(String::from(token)),
+            None => None,
+        };
+        let path = match msg_tokens.next() {
+            Some(token) => Some(String::from(token)),
+            None => None,
+        };
+        let protocol = match msg_tokens.next() {
+            Some(token) => Some(String::from(token)),
+            None => None,
+        };
+
+        Ok(Self {
+            method,
+            path,
+            protocol,
+        })
+    }
+}
+
+fn main() {
+    // Start the origin server
+    let port = 3000;
+    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+    let connection_listener = TcpListener::bind(socket_addr).unwrap();
+
+    println!("Running on port: {}", port);
+
+    for stream in connection_listener.incoming() {
+        // Read the first line of incoming HTTP request
+        // and covert it into RequestLine struct
+        let mut stream = stream.unwrap();
+        let mut buffer = [0; 200];
+        stream.read(&mut buffer).unwrap();
+        let req_line = "";
+        let string_request_line =
+            if let Some(line) = str::from_utf8(&buffer).unwrap().lines().next() {
+                line
+            } else {
+                println!("Invalid request line received");
+                req_line
+            };
+        let req_line = RequestLine::from_str(string_request_line).unwrap();
+
+        // Construct the HTTP response string
+        let html_response_string;
+        let order_status;
+
+        println!("len is {}", req_line.get_order_number().len());
+        println!("{:?}", req_line.get_order_number());
+
+        if req_line.method() != "GET"
+            || !req_line.path().starts_with("/order/status")
+            || req_line.get_order_number().len() == 0
+        {
+            if req_line.get_order_number().len() == 0 {
+                order_status = format!("Please provide valid order number");
+            } else {
+                order_status = format!("Sorry, this page is not found");
+            }
+
+            html_response_string = format!(
+                "HTTP/1.1 404 Not Found\nContent-type:
+                    text/html\nContent-Length:{}\n\n{}",
+                order_status.len(),
+                order_status
+            );
+        } else {
+            order_status = format!(
+                "Order status for order numebr {} is: Shipped\n",
+                req_line.get_order_number()
+            );
+            html_response_string = format!(
+                "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:{}\n\n{}",
+                order_status.len(),
+                order_status
+            );
+        }
+        stream.write(html_response_string.as_bytes()).unwrap();
     }
 }
